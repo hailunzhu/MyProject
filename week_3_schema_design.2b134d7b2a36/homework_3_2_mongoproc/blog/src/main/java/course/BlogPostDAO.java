4db5fb90232f3b247dc6d97ff480026b60aa6d0a
hailun <hailunzhu@hailun.wlan.netapp.com>
2015-06-10 14:19:57 -0700

    blog Mongodb

--- week_3_schema_design.2b134d7b2a36/homework_3_2_mongoproc/blog/src/main/java/course/BlogPostDAO.java
+++ week_3_schema_design.2b134d7b2a36/homework_3_2_mongoproc/blog/src/main/java/course/BlogPostDAO.java
@@ -22,8 +22,6 @@ public class BlogPostDAO {
     // Return a single post corresponding to a permalink
     public Document findByPermalink(String permalink) {
 
-        // XXX HW 3.2,  Work Here
-
         Document post = null;
         post =postsCollection.find(eq("permalink", permalink)).first();
         System.out.println("findby permalink");
@@ -36,7 +34,6 @@ public class BlogPostDAO {
     // how many posts are returned.
     public List<Document> findByDateDescending(int limit) {
 
-        // XXX HW 3.2,  Work Here
         // Return a list of DBObjects, each one a post from the posts collection
         List<Document> posts = null;
 
@@ -53,17 +50,6 @@ public class BlogPostDAO {
         String permalink = title.replaceAll("\\s", "_"); // whitespace becomes _
         permalink = permalink.replaceAll("\\W", ""); // get rid of non alphanumeric
         permalink = permalink.toLowerCase();
-
-
-        // XXX HW 3.2, Work Here
-        // Remember that a valid post has the following keys:
-        // author, body, permalink, tags, comments, date
-        //
-        // A few hints:
-        // - Don't forget to create an empty list of comments
-        // - for the value of the date key, today's datetime is fine.
-        // - tags are already in list form that implements suitable interface.
-        // - we created the permalink for you above.
 
         // Build the post object and insert it
         Document post = new Document();
@@ -82,20 +68,9 @@ public class BlogPostDAO {
         return permalink;
     }
 
-    // White space to protect the innocent
-
-
-
     // Append a comment to a blog post
     public void addPostComment(final String name, final String email, final String body,
                                final String permalink) {
-
-        // XXX HW 3.3, Work Here
-        // Hints:
-        // - email is optional and may come in NULL. Check for that.
-        // - best solution uses an update command to the database and a suitable
-        //   operator to append the comment on to any existing list of comments
-
 
         Document comment = new Document();
 
